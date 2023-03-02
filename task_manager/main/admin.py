2f1f498f4ba97608cfd8c4ebb1e7e572a6f0f76d
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2023-03-02 08:23:57 +0300

    Setup adminpanel for models

--- task_manager/main/admin.py
+++ task_manager/main/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Task, Tag
 
-admin.site.register(User, UserAdmin)
+
+class TaskManagerAdminSite(admin.AdminSite):
+    pass
+
+
+task_manager_admin_site = TaskManagerAdminSite(name="Task manager admin")
+
+
+@admin.register(User, site=task_manager_admin_site)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'name', 'surname', 'email', 'role')
+    search_fields = ['username', 'name', 'surname', 'email', 'role']
+
+
+@admin.register(Tag, site=task_manager_admin_site)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Task, site=task_manager_admin_site)
+class TaskAdmin(admin.ModelAdmin):
+    pass
