09e30ead60bd2d2eb94c6f995fffaf9f5b886c53
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2023-03-04 15:47:35 +0300

    edit admin.py after rebase error

--- task_manager/main/admin.py
+++ task_manager/main/admin.py
@@ -2,29 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Task, Tag
 
-<<<<<<< Updated upstream
-=======
-<<<<<<< Updated upstream
+
 admin.site.register(User, UserAdmin)
-=======
->>>>>>> Stashed changes
+
 
 class TaskManagerAdminSite(admin.AdminSite):
     pass
-
 
 task_manager_admin_site = TaskManagerAdminSite(name="Task manager admin")
 
 
 @admin.register(User, site=task_manager_admin_site)
 class UserAdmin(admin.ModelAdmin):
-<<<<<<< Updated upstream
-    list_display = ('username', 'name', 'surname', 'email', 'role')
-    search_fields = ['username', 'name', 'surname', 'email', 'role']
-=======
     list_display = ("username", "name", "surname", "email", "role")
     search_fields = ["username", "name", "surname", "email", "role"]
->>>>>>> Stashed changes
 
 
 @admin.register(Tag, site=task_manager_admin_site)
@@ -34,9 +25,6 @@ class TagAdmin(admin.ModelAdmin):
 
 @admin.register(Task, site=task_manager_admin_site)
 class TaskAdmin(admin.ModelAdmin):
-<<<<<<< Updated upstream
-    pass
-=======
     list_display = (
         "title", "author_name", "author_surname", "author_email",
         "executor_name", "executor_surname", "executor_email", "created_at",
@@ -68,5 +56,3 @@ class TaskAdmin(admin.ModelAdmin):
 
     def display_tags(self, obj):
         return ", ".join([tag.title for tag in obj.tags.all()])
->>>>>>> Stashed changes
->>>>>>> Stashed changes
