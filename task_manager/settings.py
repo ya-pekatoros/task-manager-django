f654c0c123a37d73511c5995ecdfe3ff3419da7b
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2023-03-09 11:46:00 +0300

    Connect JWT auth, configure tests to work with force_login without JWT

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     "drf_yasg",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -93,6 +94,7 @@ AUTH_USER_MODEL = "main.User"
 REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    'DEFAULT_AUTHENTICATION_CLASSES': ("rest_framework_simplejwt.authentication.JWTAuthentication",),
 }
 
 # Password validation
