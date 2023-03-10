3b304be68712060b3c64f1f13d140d51a0ccb7ec
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2023-03-10 13:40:31 +0300

    Add rollbar

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -52,7 +52,15 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
+
+ROLLBAR = {
+    'access_token': 'bae07f28989d4604ad0184c099b69888',
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
 
 ROOT_URLCONF = "task_manager.urls"
 
