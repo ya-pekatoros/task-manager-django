38712218e72e727dd9b8412bb3eaa6bbe6d5d5c7
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2023-03-14 09:00:02 +0300

    add-mail-tests, black refactor

---
+++ task_manager/main/test/test_mail_service.py
@@ -0,0 +1,48 @@
+from unittest.mock import patch, MagicMock
+
+from django.core import mail
+from django.template.loader import render_to_string
+
+from task_manager.main.models import Task, User
+from task_manager.main.services.mail import send_assign_notification
+from .base import TestViewSetBase
+
+
+class TestSendEmail(TestViewSetBase):
+    user_attributes = {
+        "username": "Test-developer",
+        "name": "Test-developer",
+        "surname": "Test-developer",
+        "email": "test-developer@test.com",
+        "password": "12345",
+        "role": User.Roles.DEVELOPER,
+    }
+
+    @patch.object(mail, "send_mail")
+    def test_send_assign_notification(self, fake_sender: MagicMock) -> None:
+        assignee = self.user
+
+        task_data = {
+            "title": "Test-task",
+            "description": "Test",
+            "deadline": "2023-07-05",
+            "state": Task.States.NEW,
+            "priority": Task.Priorities.MIDDLE,
+            "author": self.user,
+            "executor": assignee,
+        }
+
+        task = self.create_task(task_data)
+
+        send_assign_notification(task.id)
+
+        fake_sender.assert_called_once_with(
+            subject="You've assigned a task.",
+            message="",
+            from_email=None,
+            recipient_list=[assignee.email],
+            html_message=render_to_string(
+                "emails/notification.html",
+                context={"task": Task.objects.get(pk=task.id)},
+            ),
+        )
