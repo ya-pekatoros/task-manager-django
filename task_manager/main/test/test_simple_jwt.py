a9553b1e4d5d9d28417b9e147374d5b380850539
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2023-03-16 13:28:46 +0300

    Upd serializers and tests - add user avatars

--- task_manager/main/test/test_simple_jwt.py
+++ task_manager/main/test/test_simple_jwt.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 
 from task_manager.main.test.base import UserFactory
 
+
 class TestJWTAuth(APITestCase):
     token_url = reverse("token_obtain_pair")
     refresh_token_url = reverse("token_refresh")
@@ -47,6 +48,7 @@ class TestJWTAuth(APITestCase):
         response = self.refresh_token_request(refresh_token)
         assert response.status_code == HTTPStatus.OK
         assert response.json()["access"]
+
     def test_token_auth(self) -> None:
         client = self.client_class()
         response = client.get(self.any_api_url)
